837c846f247408710d10a217ddd737d126701368
mirzabakti <mirza.bakti@gmail.com>
2024-07-17 13:38:03 +0700

    membuat fitur search

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -6,16 +6,28 @@ import PokemonItem from "../PokemonItem/PokemonItem";
 
 function PokemonList() {
   const [pokemons] = useState(pokemonJSON);
+  const [filterPokemons, setFilterPokemons] = useState(pokemonJSON);
+
+  const handleSearch = (e) => {
+    let search = pokemons.filter((item) => {
+      return item.name.toLowerCase().includes(e.target.value);
+    });
+
+    setFilterPokemons(search);
+  };
 
   return (
     <div>
+      <input type="text" placeholder="cari pokemon..." className="search" onChange={handleSearch} />
       <div className="list-pokemon">
-        {pokemons.map((item) => (
-          <PokemonItem
-            key={item.id}
-            pokemon={item}
-          />
-        ))}
+        {filterPokemons.length == 0 ? (
+          <div>data tidak ditemukan</div>
+        ) : (
+          filterPokemons.map((item) => (
+          <PokemonItem key={item.id} pokemon={item} />
+          ))
+        )}
+        
       </div>
     </div>
   );
