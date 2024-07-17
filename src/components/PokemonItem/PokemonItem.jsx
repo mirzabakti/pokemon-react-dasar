eb65e4e85d0c06eababa45315b9c9b445446c564
mirzabakti <mirza.bakti@gmail.com>
2024-07-17 11:05:15 +0700

    memberikan warna untuk background card diambil dari data json

--- src/components/PokemonItem/PokemonItem.jsx
+++ src/components/PokemonItem/PokemonItem.jsx
@@ -2,7 +2,12 @@ import "./PokemonItem.css";
 
 function PokemonItem({ pokemon }) {
   return (
-    <div className="pokemon-card">
+    <div
+      className="pokemon-card"
+      style={{
+        backgroundColor: `${pokemon.color}`
+      }}
+    >
       <img src={pokemon.imageUrl} alt={pokemon.name} width={250} />
       <h1>{pokemon.name}</h1>
       <p className="description">{pokemon.description}</p>
